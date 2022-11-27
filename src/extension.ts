5d5d6e7620e87ec7cc4484b031f4ea8c4b6b6b9d
Jacob <spjak1@gmail.com>
2022-11-27 17:46:23 +0100

    Npm workspace to make js import work

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,84 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import * as Markdown from 'homebrewery' 
+import * as fs from 'fs'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dungeonsandmarkdown" is now active!');
+const TEMPLATE_HTML = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css" />
+        <link href='https://homebrewery.naturalcrit.com/homebrew/bundle.css' rel='stylesheet' />
+        <base target="_blank">
+    </head>
+    <body>
+        <div>
+            <div class="frame-content">
+                <div class="brewRenderer">
+                    <link href="https://homebrewery.naturalcrit.com/themes/V3/Blank/style.css" rel="stylesheet">
+                    <link href="https://homebrewery.naturalcrit.com/themes/V3/5ePHB/style.css" rel="stylesheet">
+                    <div class="pages">
+                        {{ body }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>`
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dungeonsandmarkdown.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DungeonsAndMarkdown!');
-	});
-
-	context.subscriptions.push(disposable);
+function renderPage(pageText: string, index: number){
+	pageText += `\n\n&nbsp;\n\\column\n&nbsp;`
+	let body = `
+		<div class="page" id="p${index + 1}" key="${index}" >
+			<div class="columnWrapper">${Markdown.render(pageText)}</div>
+		</div>`
+	return body
 }
 
-// this method is called when your extension is deactivated
+function generateHTML(text: string) {
+	let pages = text.split(/^\\page$/gm)
+	let outputHtml = ""
+	for (let i = 0; i < pages.length; i++) {
+		outputHtml += renderPage(pages[i], i)
+	}
+	return outputHtml
+}
+
+function generateFile(){
+	let editor = vscode.window.activeTextEditor
+	let doc = editor?.document
+	if(!editor || doc?.languageId !== 'markdown') {
+        vscode.window.showErrorMessage('Not a valid Markdown file');
+        return;
+    }
+
+    else if(doc.isUntitled) {
+        vscode.window.showErrorMessage('Please save the file first');
+        return;
+    }
+
+    if(doc.isDirty) {
+        doc.save();
+    }
+
+	let outPath = doc.fileName.replace(/\.\w+?$/, `.html`);
+    outPath = outPath.replace(/^([cdefghij]):\\/, (match, p1) => {
+        return `${p1.toUpperCase()}:\\`; // Capitalize drive letter
+    });
+    if(!outPath.endsWith('.html')) {
+        outPath += '.html';
+    }
+
+	let text = editor?.document.getText()
+	let res = text? generateHTML(text) : null
+
+	res? fs.writeFileSync(outPath, TEMPLATE_HTML.replace('{{ body }}', res), 'utf8') : null
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let generateCommand = vscode.commands.registerCommand('dungeonsandmarkdown.generate', generateFile)
+	context.subscriptions.push(generateCommand)
+}
+
 export function deactivate() {}
